becf54689a6281eeb3146ec1509704ac215f4a93
Yuji Suehiro <yuji.sue@gmail.com>
2021-04-29 12:14:10 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='slibpy',
+   version='1.0',
+   description='Package to handle slib using Python',
+   author='Yuji Suehiro',
+   packages=['slibpy'],
+)
